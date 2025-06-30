c3337905b3c8ba7bc1ddb7a089b11add291d095d
Jack Ogaja <12779448+JackOgaja@users.noreply.github.com>
2025-06-30 13:38:14 +0200

    Added __init__.py

---
+++ tdiobench/engines/__init__.py
@@ -0,0 +1,13 @@
+"""
+Benchmark execution engines for the Tiered Storage I/O Benchmark Suite.
+
+This package contains engines for executing storage benchmarks using various
+underlying tools like FIO, dd, etc.
+
+Author: Jack Ogaja
+Date: 2025-06-26
+"""
+
+from tdiobench.engines.fio_engine import FIOEngine
+
+__all__ = ['FIOEngine']
