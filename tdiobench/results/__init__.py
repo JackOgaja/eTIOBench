17949f2b6aab570ecff8de5c591f146ba0b8c01a
Jack Ogaja <12779448+JackOgaja@users.noreply.github.com>
2025-06-30 13:31:44 +0200

    Added __init__.py

---
+++ tdiobench/results/__init__.py
@@ -0,0 +1,13 @@
+"""
+Results components for the Tiered Storage I/O Benchmark Suite.
+
+This package contains components for storing and retrieving benchmark results
+using various storage backends, including file system and databases.
+
+Author: Jack Ogaja
+Date: 2025-06-26
+"""
+
+from tdiobench.results.result_store import ResultStore
+
+__all__ = ['ResultStore']
