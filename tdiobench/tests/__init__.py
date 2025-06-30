0886bf29ffed1fd0d1f3cd7d6d4fe827e763ed2f
Jack Ogaja <12779448+JackOgaja@users.noreply.github.com>
2025-06-30 11:12:19 +0200

    Added __init__.py

---
+++ tdiobench/tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+Test package for the Tiered Storage I/O Benchmark Suite.
+
+This package contains test modules for unit testing and integration
+testing of the benchmark suite components.
+
+Author: Jack Ogaja
+Date: 2025-06-26
+"""
