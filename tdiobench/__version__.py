1eb8eaeff560f1f5cb0fec2cefc80ed5e6b6b3da
Jack Ogaja <12779448+JackOgaja@users.noreply.github.com>
2025-06-30 16:38:06 +0200

    Added __version__.py

---
+++ tdiobench/__version__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Version information for Tiered Storage I/O Benchmark Suite.
+
+This file is used to define the version of the package, which is then
+imported and used by setup.py when building the package.
+
+Author: Jack Ogaja
+Date: 2025-06-26
+"""
+
+__version__ = "1.0.0"
